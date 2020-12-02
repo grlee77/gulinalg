b3217079e20e850d7a2808f7090d0a49ec63b91b
Gregory Lee <grlee77@gmail.com>
2020-12-02 13:53:40 -0500

    BUG: fix local variable error in det

--- gulinalg/tests/test_determinant.py
+++ gulinalg/tests/test_determinant.py
@@ -20,6 +20,7 @@ class TestSlogdet(TestCase):
         a = rstate.randn(m, m)
         sign, logdet = gulinalg.slogdet(a)
         det = gulinalg.det(a)
+        assert_allclose(det, np.linalg.det(a), rtol=1e-6, atol=1e-12)
         assert_allclose(det, sign * np.exp(logdet), rtol=1e-6, atol=1e-12)
 
     def test_complex(self):
@@ -28,6 +29,7 @@ class TestSlogdet(TestCase):
         a = rstate.randn(m, m) + 1j * rstate.randn(m, m)
         sign, logdet = gulinalg.slogdet(a)
         det = gulinalg.det(a)
+        assert_allclose(det, np.linalg.det(a), rtol=1e-6, atol=1e-12)
         assert_allclose(det, sign * np.exp(logdet), rtol=1e-6, atol=1e-12)
 
     def test_real_vector(self):
@@ -37,16 +39,20 @@ class TestSlogdet(TestCase):
         for workers in [1, -1]:
             sign, logdet = gulinalg.slogdet(a, workers=workers)
             det = gulinalg.det(a, workers=workers)
-            assert_allclose(det, sign * np.exp(logdet), rtol=1e-6, atol=1e-12)
+            det_ref = np.linalg.det(a)
+            assert_allclose(det_ref, det, rtol=1e-6, atol=1e-12)
+            assert_allclose(det_ref, sign * np.exp(logdet), rtol=1e-6, atol=1e-12)
 
     def test_complex_vector(self):
         m = 3
         rstate = np.random.RandomState(123)
-        a = rstate.randn(n_batch, m, m) + 1j*rstate.randn(n_batch, m, m)
+        a = rstate.randn(n_batch, m, m) + 1j * rstate.randn(n_batch, m, m)
         for workers in [1, -1]:
             sign, logdet = gulinalg.slogdet(a, workers=workers)
             det = gulinalg.det(a, workers=workers)
-            assert_allclose(det, sign * np.exp(logdet), rtol=1e-6, atol=1e-12)
+            det_ref = np.linalg.det(a)
+            assert_allclose(det_ref, det, rtol=1e-6, atol=1e-12)
+            assert_allclose(det_ref, sign * np.exp(logdet), rtol=1e-6, atol=1e-12)
 
 
 if __name__ == '__main__':
